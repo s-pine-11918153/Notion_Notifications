93d48cd8365e7557c98049c7e2e449ae14fff29b
s-pine-11918153 <115201750+s-pine-11918153@users.noreply.github.com>
2025-11-09 12:06:23 +0900

    check_notion.py を更新

--- check_notion.py
+++ check_notion.py
@@ -116,7 +116,7 @@ def cleanup_old_workflow_runs():
     runs_resp.raise_for_status()
     runs = runs_resp.json().get("workflow_runs", [])
 
-    for run in runs[10:]:
+    for run in runs[2:]:
         run_id = run["id"]
         del_resp = requests.delete(
             f"https://api.github.com/repos/{REPO}/actions/runs/{run_id}",
